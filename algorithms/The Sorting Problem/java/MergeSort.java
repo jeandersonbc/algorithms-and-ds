b48b12ecfcf7b2321af0372fe6daf91991fd2b03
Jeanderson Candido <jeandersonbc@gmail.com>
2014-10-18 20:21:46 -0300

    Sorting - Added MergeSort implementation

---
+++ algorithms/The Sorting Problem/java/MergeSort.java
@@ -0,0 +1,86 @@
+import java.lang.Comparable;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * The Merge-Sort algorithm uses a Divide And Conquer approach.
+ *
+ * Characteristics:
+ *     Running time: Linear-Logarithmic BigOh(N * log(N))
+ *     This algorithm requires extra memory to solve the sorting problem. Although
+ *     It is faster than Insertion Sort for large sequences, the overhead of the
+ *     Merge Sort for small sequences is much more than required by the Insertion
+ *     Sort. A good optimization would be using Insertion Sort when the size of the
+ *     sequence is small enough.
+ *
+ * Recurrence Function:
+ *     T(n) = 2 * T(n / 2) + f(n)
+ *
+ * Reference:
+ *     CLRS 3rd Edition - Section 2.3.1
+ */
+public class MergeSort<T extends Comparable<T>> implements SortingAlg<T> {
+
+    private List<T> buffer;
+
+    // wrapper function
+    @Override
+    public void sort(List<T> elements) {
+        if (elements == null)
+            return;
+
+        // Give enough space to avoid having to keep resizing.
+        // Remember that by doing this, buffer.size() == 0
+        this.buffer = new ArrayList<T>(elements.size());
+
+        sort(elements, 0, elements.size() - 1);
+        this.buffer = null;
+    }
+    private void sort(List<T> elements, int min, int max) {
+        if (min == max)
+            return;
+
+        int mid = (min + max) >> 1;
+        sort(elements, min, mid);
+        sort(elements, mid + 1, max);
+
+        merge(elements, min, mid, max);
+    }
+    private void merge(List<T> elements, int min, int mid, int max) {
+        if (lessThanOrEquals(elements.get(mid), elements.get(mid+1)))
+            return;
+
+        for (int k = min; k <= max; k++)
+            this.buffer.add(k, elements.get(k));
+
+        int left = min;
+        int right = mid + 1;
+        for (int k = min; k <= max; k++) {
+            if (left > mid)
+                elements.set(k, this.buffer.get(right++));
+            else if (right > max)
+                elements.set(k, this.buffer.get(left++));
+            else if (lessThan(this.buffer.get(left), this.buffer.get(right)))
+                elements.set(k, this.buffer.get(left++));
+            else
+                elements.set(k, this.buffer.get(right++));
+        }
+    }
+    // Helper functions
+    private boolean lessThanOrEquals(T first, T second) {
+        return (first.compareTo(second) <= 0);
+    }
+    private boolean lessThan(T first, T second) {
+        return (first.compareTo(second) < 0);
+    }
+    // Simple driver
+    public static void main(String[] args) {
+        SortingAlg<Integer> alg = new MergeSort<Integer>();
+        List<Integer> elems = java.util.Arrays.asList(9,5,2,7,3,8,2,1,4,7,5,3,9);
+        alg.sort(elems);
+        for (int i = 0; i < elems.size() - 1; i++) {
+            if (elems.get(i).compareTo(elems.get(i+1)) > 0)
+                throw new java.lang.RuntimeException("Elems should be sorted.");
+        }
+        System.out.println("All tests passed.");
+    }
+}
