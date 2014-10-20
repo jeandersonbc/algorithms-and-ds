8ddd0d1d48494067fc2da12b57c64104b9f60b48
Jeanderson Candido <jeandersonbc@gmail.com>
2014-10-20 00:12:52 -0200

    Updated copyright line for each file

--- algorithms/The Sorting Problem/java/SortingAlg.java
+++ algorithms/The Sorting Problem/java/SortingAlg.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright (C) 2013, 2014  Jeanderson Barros Candido
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Contributors:
+ *     Jeanderson Candido <http://jeandersonbc.github.io> - Initial Implementation
+ */
 import java.lang.Comparable;
 import java.util.List;
 /**
  * Interface for the Sorting problem. It is convenient to use the Comparable interface.
- * @author Jeanderson Candido - http://jeandersonbc.github.io
  * @param T - Type of the objects to be sorted
  */
 public interface SortingAlg<T extends Comparable<T>> {
