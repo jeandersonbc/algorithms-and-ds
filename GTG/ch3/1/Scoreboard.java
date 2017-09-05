b07de077bf9b233faa323b724fed5aeac9e9a73c
Jeanderson Candido <jeandersonbc@gmail.com>
2017-09-05 18:39:14 -0300

    Handling program input

--- GTG/ch3/1/Scoreboard.java
+++ GTG/ch3/1/Scoreboard.java
@@ -10,7 +10,16 @@ class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedInputStream(System.in));
+        int testcases = in.nextInt();
+        while (testcases-- > 0) {
+            int capacity = in.nextInt();
+            int entries = in.nextInt();
 
+            for (int e = 0; e < entries; e++) {
+                String player = in.next();
+                int score = in.nextInt();
+            }
+        }
         in.close();
     }
 }
