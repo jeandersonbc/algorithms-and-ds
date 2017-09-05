d428477cc7fd196869ab8e0642f93ef6ef5a8f50
Jeanderson Candido <jeandersonbc@gmail.com>
2017-09-05 18:31:22 -0300

    Started some hands-on from new book

---
+++ GTG/ch3/1/Scoreboard.java
@@ -0,0 +1,16 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/*
+ * Implementation based on the scoreboard problem from Sec 3.1
+ *
+ * Author: Jeanderson Candido <http://jeandersonbc.github.io>
+ */
+class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+
+        in.close();
+    }
+}
