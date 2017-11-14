028d4fdeebaeed3df87818be08b0a16f6c43f701
Jeanderson Candido <jeandersonbc@gmail.com>
2017-11-14 00:30:09 -0300

    Kickoff on training sessions

---
+++ training/kickoff/Sum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+        sc.close();
+    }
+}
