bf2f0cad400fbf497349a8f7d64093942c64b773
Jeanderson Candido <jeandersonbc@gmail.com>
2017-12-26 11:05:41 -0300

    Added some tests and benchmarks

--- training/RecursiveCoinChange.java
+++ training/RecursiveCoinChange.java
@@ -1,8 +1,16 @@
 import java.util.Arrays;
 
+/*
+ * Given an arbitrary amount of money and a set of coin denominations,
+ * do you know how to compute the minimum number of coins necessary
+ * to change the informed amount of money?
+ *
+ * For instance, given $25 and the coins $1, $5, and $10, you can change
+ * $25 with at least 3 coins: 10+10+5
+ */
 class Main {
 
-    /* This recursive approach solves the problem corretly
+    /* This recursive approach solves the problem correctly
      * but it does not scale for large inputs.
      *
      * Can you guess why? :)
@@ -41,23 +49,44 @@ class Main {
         return minCoins[value];
     }
 
+    static void correctness(int testValue, int[] testCoins) {
+        System.out.printf("Test Value: %d, Test Coins: %s\n", testValue, Arrays.toString(testCoins));
+        int sol1 = solve(testValue, testCoins);
+        int sol2 = faster(testValue, testCoins);
+        if (sol1 != sol2) {
+            System.err.printf("Wrong Answer - Recursive: %d, DP: %d\n", sol1, sol2);
+        } else {
+            System.out.printf("Correct Answer: %d\n", sol2);
+        }
+    }
+
+    static void efficiency(int minValue, int maxValue, int[] testCoins) {
+        System.out.println("N\tNaive\tDP");
+        for (int testValue = minValue; testValue <= maxValue; testValue += 10) {
+            long t1 = System.currentTimeMillis();
+            solve(testValue, testCoins);
+            t1 = System.currentTimeMillis() - t1;
+
+            long t2 = System.currentTimeMillis();
+            faster(testValue, testCoins);
+            t2 = System.currentTimeMillis() - t2;
+
+            System.out.printf("%d\t%d\t%d\n", testValue, t1, t2);
+        }
+    }
+
+    // Some tests...
     public static void main(String[] args) {
-        int value = 25;
-        int[] coins = {5, 10, 15};
-        System.out.println(value + " " + Arrays.toString(coins));
-        System.out.println(solve(value, coins));
-        System.out.println(faster(value, coins));
+        correctness(25, new int[]{5, 10, 15});
+        correctness(40, new int[]{5, 10, 25, 50});
+        correctness(40, new int[]{5, 10, 20, 25, 50});
+        correctness(7, new int[]{1, 3, 4, 5});
 
-        value = 40;
-        coins = new int[]{5, 10, 25, 50};
-        System.out.println(value + " " + Arrays.toString(coins));
-        System.out.println(solve(value, coins));
-        System.out.println(faster(value, coins));
+        System.out.println("----------");
+        efficiency(100, 150, new int[]{5, 10, 20, 25, 50});
 
-        value = 40;
-        coins = new int[]{5, 10, 20, 25, 50};
-        System.out.println(value + " " + Arrays.toString(coins));
-        System.out.println(solve(value, coins));
-        System.out.println(faster(value, coins));
+        System.out.println("----------");
+        int sol = faster(10_000_000, new int[]{5, 10, 20, 25, 50});
+        System.out.printf("For N = 10^7, Coin Change = %d\n", sol);
     }
 }
